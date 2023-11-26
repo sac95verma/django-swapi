91fef9fb42959776afb0e4b0a8aaf68dbabde729
sac95verma <sac95verma@gmail.com>
2023-11-26 17:54:39 +0400

    fix admin page

--- swapi/middleware.py
+++ swapi/middleware.py
@@ -6,16 +6,12 @@ class SecretTokenMiddleware:
 
     def __call__(self, request):
         secret_token = 'your_secret_token'  # Replace with your actual secret token
-        excluded_paths = ['/admin/', '/swapi/people/rate']
-        if request.path not in excluded_paths:
+        excluded_paths = ['/swapi/people/rate']
+        if request.path in excluded_paths:
         # Check if the secret token is present in the Authorization header
             authorization_header = request.headers.get('Authorization', '')
-            if authorization_header == f'Token {secret_token}':
-                # Token is valid, proceed with the request
-                response = self.get_response(request)
-            else:
-                # Token is missing or invalid, reject the request
+            if authorization_header != f'Token {secret_token}':
                 response = HttpResponseForbidden()
-            return response
-        
+                return response
+
         return self.get_response(request)
